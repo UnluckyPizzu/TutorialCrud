d6e4cdf9b95c738e49d27d64012042b85ee89bb9
unluckypizzu <emiliopizzuto99@gmail.com>
2023-07-13 09:55:19 +0200

    Before and after example

--- src/main/java/com/pizzu/tutorial/aspect/ServiceAspect.java
+++ src/main/java/com/pizzu/tutorial/aspect/ServiceAspect.java
@@ -1,20 +1,55 @@
 package com.pizzu.tutorial.aspect;
 
+import java.util.List;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import com.pizzu.tutorial.model.Tutorial;
 
 @Aspect
+@Component
 public class ServiceAspect {
+	
+	
+	@Pointcut("execution(* com.pizzu.tutorial.service.*.findAll*(..))")
+	public void forFindAllServicePointcuts() {}
+	
 	
 	@Pointcut("execution(* com.pizzu.tutorial.service.*.*(..))")
 	public void forServicePointcuts() {}
 	
 	@Before("forServicePointcuts()")
-	public void beforeLogging( ) {
-		System.out.println("prova");
+	public void beforeLogging(JoinPoint jointPoint) {
+		
+		MethodSignature signature = (MethodSignature) jointPoint.getSignature();
+		Object[] args = jointPoint.getArgs();
+		System.out.println("Method: " + signature);
+		
+		if(args.length != 0)
+			System.out.println("Parametri: ");
+		
+		for (Object object : args) {
+			System.out.println(object.toString());
+		}
+	}
+	
+	@AfterReturning(pointcut = "forFindAllServicePointcuts()", returning = "tutorials")
+	public void afterLogging(JoinPoint jointPoint, List<Tutorial> tutorials) {
+		
+		MethodSignature signature = (MethodSignature) jointPoint.getSignature();
+		
+		if(!tutorials.isEmpty())
+			System.out.println("Return: ");
+		
+		for (Tutorial tutorial : tutorials) {
+			System.out.println(tutorial.toString());
+		}
 	}
 
 }
